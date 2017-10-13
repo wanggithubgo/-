16e4f1c04a6652791e46551196f3cc471c33b033
作业 <31690369+wanggithubgo@users.noreply.github.com>
2017-10-13 17:22:08 +0800

    Add files via upload

---
+++ exer-3-8.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+void main()
+{
+int a,b,i,n;
+cin>>a>>b;
+i=a/b;
+n=a%b;
+if(a%b==0)
+cout<<a<<"/"<<b<<"="<<i<<endl;
+else
+cout<<a<<"/"<<b<<"="<<i<<endl<<"ำเสýสว:"<<n<<endl;
+}
